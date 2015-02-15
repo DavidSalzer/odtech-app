507bdebbb1727cef0f966d76c8d98d3c540a785f
shaigiv <gshai@cambium.co.il>
2015-02-15 11:04:50 +0200

    android work

---
+++ platforms/android/assets/www/features/multiQuestion/multiQuestion.js
@@ -0,0 +1,11 @@
+odtechApp.directive('multiQuestion', [ function () {
+    return {
+        restrict: 'E',
+        templateUrl: './features/multiQuestion/multiQuestion.html',
+        link: function (scope, el, attrs) {           
+        },
+        replace: true
+    };
+
+
+} ]);
