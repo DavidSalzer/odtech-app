728b923b6655664e8f3b57f30656a67cd1b682ba
gidi <rgidi@cambium.co.il>
2015-02-19 16:45:01 +0200

    delete phonegap geo location plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -85,34 +85,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
-        "id": "org.apache.cordova.geolocation.Coordinates",
-        "clobbers": [
-            "Coordinates"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
-        "id": "org.apache.cordova.geolocation.PositionError",
-        "clobbers": [
-            "PositionError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
-        "id": "org.apache.cordova.geolocation.Position",
-        "clobbers": [
-            "Position"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
-        "id": "org.apache.cordova.geolocation.geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.file.DirectoryEntry",
         "clobbers": [
@@ -265,7 +237,6 @@ module.exports.metadata =
     "org.apache.cordova.camera": "0.3.5",
     "org.apache.cordova.media-capture": "0.3.7-dev",
     "com.phonegap.plugins.barcodescanner": "2.0.1",
-    "org.apache.cordova.geolocation": "0.3.12",
     "org.apache.cordova.file": "1.3.3"
 }
 // BOTTOM OF METADATA
