69cde5797c2d59b15c8a9fc5a45775631fada034
Einat Kidron <keinat@cambium.co.il>
2018-06-06 15:59:02 +0300

    changes made in ybz due to google demanding use of api keys for maps. In mapDisplay the api keys are for ios and android seperatly and for ybz: check if can be used for other apps as well. For map mission a general api key for both platforms for odtech apps - check if can be used for all apps as well.

--- www/components/mapDisplay/mapDisplay.js
+++ www/components/mapDisplay/mapDisplay.js
@@ -1,169 +1,171 @@
-odtechApp.controller('mapDisplay', ['$rootScope', '$scope', '$state', 'missions', '$timeout', 'server', function ($rootScope, $scope, $state, missions, $timeout, server) {
-    map != undefined ? map.clear() : '';
-    map != undefined ? map.off() : '';
-
+odtechApp.controller('mapDisplay', [
+  '$rootScope',
+  '$scope',
+  '$state',
+  'missions',
+  '$timeout',
+  'server',
+  function($rootScope, $scope, $state, missions, $timeout, server) {
     $scope.showLive = true;
     $scope.showLoadStripe = false;
     var markers = [];
     //add markers to map
-    $scope.addMarkers = function (data, callback) {
-
-        function onMarkerAdded(marker) {
-            markers.push(marker);
-            if (markers.length === data.length) {
-                callback(markers);
-            }
+    $scope.addMarkers = function(data, callback) {
+      function onMarkerAdded(marker) {
+        markers.push(marker);
+        if (markers.length === data.length) {
+          callback(markers);
         }
-        data.forEach(function (markerOptions, index) {
-            var icon = markerOptions.stationStatus == 0 ? $rootScope.imgDomain + 'site/img/' + (index * 1 + 1) + '.png' : $rootScope.imgDomain + 'site/img/mapDisPointDone.png';
-            if (markerOptions && markerOptions.data) {
-                map.addMarker({
-                    'position': new plugin.google.maps.LatLng(markerOptions.data.latitude, markerOptions.data.longitude),
-                    //'title': "Hello",
-                    'icon': {
-                        url: icon
-                    },
-                    'styles': {
-                        'color': 'red'
-                    }
-                }, function (marker) {
-                    marker.addEventListener(plugin.google.maps.event.MARKER_CLICK, function () {
-                        //marker.showInfoWindow();
-                        $scope.placeMarker(markerOptions)
-                    });
-
-
-
-
-
-
-                });
-            }
-
-        });
-    }
-
-    //in map loaded event -set the map options
-    $scope.onMapLoaded = function () {
-
-
-
-        //set map options
-        map.setOptions({
-            'backgroundColor': 'white',
-            'mapType': plugin.google.maps.MapTypeId.ROADMAP,
-            'controls': {
-                'myLocationButton': true
+      }
+      data.forEach(function(markerOptions, index) {
+        var icon =
+          markerOptions.stationStatus == 0
+            ? $rootScope.imgDomain + 'site/img/' + (index * 1 + 1) + '.png'
+            : $rootScope.imgDomain + 'site/img/mapDisPointDone.png';
+        if (markerOptions && markerOptions.data) {
+          $rootScope.map.addMarker(
+            {
+              position: new plugin.google.maps.LatLng(
+                markerOptions.data.latitude,
+                markerOptions.data.longitude
+              ),
+              icon: {
+                url: icon
+              },
+              styles: {
+                color: 'red'
+              }
             },
-            'camera': {
-                'latLng': $scope.centerLatLng,
-                'zoom': $.browser.isSmartphone ? 17 : 19
-
+            function(marker) {
+              marker.addEventListener(
+                plugin.google.maps.event.MARKER_CLICK,
+                function() {
+                  //marker.showInfoWindow();
+                  $scope.placeMarker(markerOptions);
+                }
+              );
             }
-        });
-        //add markers. this way is important because of the ansychronalize between js and native
-        $scope.addMarkers($scope.stages, function (markers) {
-        });
+          );
+        }
+      });
+    };
 
-
-
-
-    }
+    $scope.onMapLoaded = function() {
+      console.log('MAP IS loaded event?');
+      $rootScope.map.setClickable($scope.showLive);
+      //add markers. this way is important because of the ansychronalize between js and native
+      $scope.addMarkers($scope.stages, function(markers) {});
+      $scope.showLoadStripe = false;
+    };
     $scope.showLoadStripe = true;
     //get the markers data - stages
-    server.request({ "type": "getActivitiesOfAppUserTwo", "req": {} })
-                                    .then(function (data) {
+    server
+      .request({ type: 'getActivitiesOfAppUserTwo', req: {} })
+      .then(function(data) {
+        $scope.stages = data.res.activities;
+        //set the index for pins' numbers
+        for (var i = 0; i < $scope.stages.length; i++) {
+          $scope.stages[i].index = i;
+        }
+        //set the center point - get the middle point. if there is no data- take another
+        $scope.centerLatLng = -1;
+        //set the middle point
+        var stagesIndex = Math.floor($scope.stages.length / 2);
+        //pass over the array and get the first poiint, from the middle, that has lang & lat
+        while ($scope.centerLatLng == -1 && $scope.stages[stagesIndex]) {
+          //if the point has lang & lat - set the centerLatLng variable
+          if (
+            $scope.stages[stagesIndex] &&
+            $scope.stages[stagesIndex].data &&
+            $scope.stages[stagesIndex].data.latitude &&
+            $scope.stages[stagesIndex].data.longitude
+          ) {
+            $scope.centerLatLng = new plugin.google.maps.LatLng(
+              $scope.stages[stagesIndex].data.latitude,
+              $scope.stages[stagesIndex].data.longitude
+            );
+          } else {
+            stagesIndex++;
+          }
+        }
+        //if there is no lang & lat point
+        if ($scope.centerLatLng == -1) {
+          stagesIndex = 0;
+          while (
+            $scope.centerLatLng == -1 &&
+            $scope.stages[stagesIndex] &&
+            stagesIndex < Math.floor($scope.stages.length / 2)
+          ) {
+            //if the point has lang & lat - set the centerLatLng variable
+            if (
+              $scope.stages[stagesIndex] &&
+              $scope.stages[stagesIndex].data &&
+              $scope.stages[stagesIndex].data.latitude &&
+              $scope.stages[stagesIndex].data.longitude
+            ) {
+              $scope.centerLatLng = new plugin.google.maps.LatLng(
+                $scope.stages[stagesIndex].data.latitude,
+                $scope.stages[stagesIndex].data.longitude
+              );
+            } else {
+              stagesIndex++;
+            }
+          }
+        }
 
+        /*** init the map ***/
+        var myOptions = {
+          backgroundColor: 'white',
+          mapType: plugin.google.maps.MapTypeId.ROADMAP,
 
-                                        $scope.stages = data.res.activities;
-                                        //set the index for pins' numbers
-                                        for (var i = 0; i < $scope.stages.length; i++) {
-                                            $scope.stages[i].index = i
-                                        }
-                                        //set the center point - get the middle point. if there is no data- take another
-                                        $scope.centerLatLng = -1;
-                                        //set the middle point
-                                        var stagesIndex = Math.floor($scope.stages.length / 2);
-                                        //pass over the array and get the first poiint, from the middle, that has lang & lat
-                                        while ($scope.centerLatLng == -1 && $scope.stages[stagesIndex]) {
-                                            //if the point has lang & lat - set the centerLatLng variable
-                                            if ($scope.stages[stagesIndex] && $scope.stages[stagesIndex].data && $scope.stages[stagesIndex].data.latitude && $scope.stages[stagesIndex].data.longitude) {
-                                                $scope.centerLatLng = new plugin.google.maps.LatLng($scope.stages[stagesIndex].data.latitude, $scope.stages[stagesIndex].data.longitude);
-                                            }
-                                            else {
-                                                stagesIndex++;
-                                            }
-                                        }
-                                        //if there is no lang & lat point
-                                        if ($scope.centerLatLng == -1) {
-                                            stagesIndex = 0
-                                            while ($scope.centerLatLng == -1 && $scope.stages[stagesIndex] && stagesIndex < Math.floor($scope.stages.length / 2)) {
-                                                //if the point has lang & lat - set the centerLatLng variable
-                                                if ($scope.stages[stagesIndex] && $scope.stages[stagesIndex].data && $scope.stages[stagesIndex].data.latitude && $scope.stages[stagesIndex].data.longitude) {
-                                                    $scope.centerLatLng = new plugin.google.maps.LatLng($scope.stages[stagesIndex].data.latitude, $scope.stages[stagesIndex].data.longitude);
-                                                }
-                                                else {
-                                                    stagesIndex++;
-                                                }
-                                            }
-                                        }
+          camera: {
+            latLng: $scope.centerLatLng,
+            zoom: $.browser.isSmartphone ? 17 : 19
+          }
+        };
+        $rootScope.map = plugin.google.maps.Map.getMap(
+          document.getElementById('map_canvas'),
+          myOptions
+        );
 
-
-                                        /*** init the map ***/
-                                        var mapDiv = document.getElementById("map_canvas");
-                                        // Initialize the map plugin
-                                        map = plugin.google.maps.Map.getMap(mapDiv);
-                                        // You have to wait the MAP_READY event.
-                                        map.on(plugin.google.maps.event.MAP_READY, $scope.onMapLoaded);
-                                        map.on(plugin.google.maps.event.CAMERA_IDLE, function () {
-
-                                            $scope.showLoadStripe = false;
-                                        })
-                                        map.on(plugin.google.maps.event.MAP_LOADED, function () {
-
-                                            $scope.showLoadStripe = false;
-                                        })
-
-                                    })
-
+        // Initialize the map plugin
+        // You have to wait the MAP_READY event.
+        $rootScope.map.on(
+          plugin.google.maps.event.MAP_READY,
+          $scope.onMapLoaded
+        );
+      });
 
     //go to stage by marker click
-    $scope.placeMarker = function (stageData) {
-        $rootScope.currentStageData = stageData;
-        $state.transitionTo('mainNav', { stageId: stageData.aid });
-    }
+    $scope.placeMarker = function(stageData) {
+      $rootScope.currentStageData = stageData;
+      $state.transitionTo('mainNav', { stageId: stageData.aid });
+    };
 
-    //whenb menu opened or closed
-    $scope.$on('toggleMenu', function (event, data) {
-        //if the menu open
-        if (data.isShow == true) {
-            //replace the map to image and hide the map
-            map.toDataURL(function (imageData) {
-                var image = document.getElementById("map_static");
-
-                image.src = imageData;
-                $timeout(function () {
-                    $scope.showLive = false;
-                }, 0)
-                //$("#map_canvas").hide();
-                //$("#map_static").show();
-
-            });
+    $scope.$on('menuClosed', function(event, data) {
+      $state.transitionTo(
+        $state.current,
+        {},
+        {
+          reload: true
         }
-        else {
-
-            $timeout(function () {
-                $scope.showLive = true;
-            }, 0)
-
-
-
-            //$("#map_canvas").show();
-            //$("#map_static").hide();
-        }
+      );
     });
 
-
-
-} ]);
-var map;
+    //whenb menu opened or closed
+    $scope.$on('toggleMenu', function(event, data) {
+      //if the menu open
+      if (data.isShow == true) {
+        $timeout(function() {
+          $rootScope.map.setClickable(false);
+          $scope.showLive = false;
+        }, 0);
+      } else {
+        $timeout(function() {
+          $rootScope.map.setClickable(true);
+          $scope.showLive = true;
+        }, 0);
+      }
+    });
+  }
+]);
--- www/deviceLoction.js
+++ www/deviceLoction.js
@@ -1,77 +1,81 @@
 //Check device location and send to server.
-odtechApp.run(['$rootScope', 'server', '$q', '$interval', function ($rootScope, server, $q, $interval) {
-
+odtechApp.run([
+  '$rootScope',
+  'server',
+  '$q',
+  '$interval',
+  function($rootScope, server, $q, $interval) {
     var watchLocation;
     var sendLocation;
     $rootScope.lngLocation;
     $rootScope.latLocation;
     //Start watcher for location.
-    $rootScope.$on('startLocationWatcher', function (event, data) {
-        //Clear old interval and watch position.
-        if (sendLocation) {
-            $interval.cancel(sendLocation);
-        }
-        if (watchLocation) {
-            navigator.geolocation.clearWatch(watchLocation);
-        }
+    $rootScope.$on('startLocationWatcher', function(event, data) {
+      //Clear old interval and watch position.
+      if (sendLocation) {
+        $interval.cancel(sendLocation);
+      }
+      if (watchLocation) {
+        navigator.geolocation.clearWatch(watchLocation);
+      }
 
-        //Geolocation watch position.
-        if (navigator.geolocation) {
-            watchLocation = navigator.geolocation.watchPosition($rootScope.showPosition, $rootScope.errorGetLocation, {maximumAge : 3000, enableHighAccuracy: true});
-        } else {
+      //Geolocation watch position.
+      if (navigator.geolocation) {
+        watchLocation = navigator.geolocation.watchPosition(
+          $rootScope.showPosition,
+          $rootScope.errorGetLocation,
+          { maximumAge: 3000, enableHighAccuracy: true }
+        );
+      } else {
+      }
 
-        }
-
-        //All x time, send device location to server.
-        sendLocation = $interval(function () {
-            console.log('position sent ' + pos);
-            if (pos && pos.coords) {
-                locationRequest = {
-                    type: "updateLocation",
-                    req: {
-                        latitude: pos.coords.latitude,
-                        longitude: pos.coords.longitude
-                    }
-                }
-                server.request(locationRequest)
-                .then(function (data) {
-
-                });
+      //All x time, send device location to server.
+      sendLocation = $interval(function() {
+        //  console.log('position sent ' + pos);
+        if (pos && pos.coords) {
+          locationRequest = {
+            type: 'updateLocation',
+            req: {
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude
             }
-        }, 30000);
+          };
+          server.request(locationRequest).then(function(data) {});
+        }
+      }, 30000);
     });
 
     //Success callback of watch position.
-    $rootScope.showPosition = function (loc) {
-        console.log('watcher success ' + loc);
+    $rootScope.showPosition = function(loc) {
+      // console.log('watcher success ' + loc);
 
-        pos = loc;
-        $rootScope.lngLocation = loc.coords.longitude;
-        $rootScope.latLocation = loc.coords.latitude;
-        //   console.log(new Date());
-        //   console.log(loc);
-    }
+      pos = loc;
+      $rootScope.lngLocation = loc.coords.longitude;
+      $rootScope.latLocation = loc.coords.latitude;
+      //   console.log(new Date());
+      //   console.log(loc);
+    };
 
     //Error callback of watch position.
-    $rootScope.errorGetLocation = function (err) {
-        console.log('watcher failed ' + err);
+    $rootScope.errorGetLocation = function(err) {
+      console.log('watcher failed ' + err);
 
-        pos = err;
-        $rootScope.lngLocation = -1;
-        $rootScope.latLocation = -1;
-        console.log(new Date());
-        console.log(err);
-    }
+      pos = err;
+      $rootScope.lngLocation = -1;
+      $rootScope.latLocation = -1;
+      console.log(new Date());
+      console.log(err);
+    };
 
     //Stop watcher for location.
-    $rootScope.$on('stopLocationWatcher', function (event, data) {
-        if (sendLocation) {
-            $interval.cancel(sendLocation);
-        }
-        if (watchLocation) {
-            navigator.geolocation.clearWatch(watchLocation);
-            watchLocation = null;
-        }
+    $rootScope.$on('stopLocationWatcher', function(event, data) {
+      if (sendLocation) {
+        $interval.cancel(sendLocation);
+      }
+      if (watchLocation) {
+        navigator.geolocation.clearWatch(watchLocation);
+        watchLocation = null;
+      }
     });
-
-} ]);
+  }
+]);
